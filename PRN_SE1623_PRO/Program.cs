a8a872aeb9add21bf4487a4fda6c606fe447d0f5
kirizenju <quang.com.vn1911@gmail.com>
2022-05-19 12:59:07 +0700

    Add project files.

---
+++ PRN_SE1623_PRO/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace fptedu.se1623;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($"Hello World....");
+        int x = 10,y = 20;
+        string name;
+        var salary = 10.5;
+        dynamic fullName = 10;
+        Console.WriteLine(salary.GetType);
+        Console.WriteLine($"sum of a={x} and b={y} is:{Sum(x, y)}");
+        Console.ReadLine();
+    }
+    public  static int Sum(int a,int b)
+    {
+        return a + b;
+    }
+}
